7add558f88c5a47421607e28350c9fa56e419382
yifan757904 <yifan757904@qq.com>
2025-07-21 11:23:13 +0800

    Delete model directory

--- model/enrollment.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Enrollment struct {
-	CourseID  string `gorm:"primarykey;type:varchar(20)"`
-	StudentID string `gorm:"primarykey;type:varchar(20)"`
-}
--- model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	ID   string `gorm:"primarykey;type:varchar(20)"`
-	Name string `gorm:"type:varchar(60);not null"`
-	Rule string `gorm:"type:enum('student','teacher');not null"`
-
-	Courses []Course `gorm:"many2many:enrollments;foreignKey:ID;joinForeignKey:StudentID;References:ID;joinReferences:CourseID"`
-}
